3e5c887adc1db07e8db4f4cad8235840ad135675
luan-h-santos <luan94775@gmail.com>
2025-10-01 21:33:30 -0300

    Add Luan H Santos

--- pessoa.py
+++ pessoa.py
@@ -8,4 +8,5 @@ pessoas = {
     "cane" : Pessoa("Rodrigo Cane", "https://github.com/rodrigocane", "https://m.media-amazon.com/images/M/MV5BMTY4NTQzMDcxMl5BMl5BanBnXkFtZTcwNTEwNTYyMg@@._V1_.jpg"),
     "maria": Pessoa("Maria", "https://github.com/mariazinha", "https://i.pravatar.cc/300?img=47"),
     "anonimo": Pessoa("Anônimo", "https://github.com/anonimo"),
+    'Luan_H_Santos': Pessoa('Luan H. Santos', 'https://github.com/Luan-H-Santos', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKzLOK6VU1SifpgzHrHf6YsGIvdMl1O_cu-Q&s'),
 }
